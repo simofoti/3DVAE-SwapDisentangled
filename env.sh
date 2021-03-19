0cb0c7f8837fe2dcfa55df2f1d6b40a6fcf3b001
Simone <simofoti@gmail.com>
2021-03-19 21:30:28 +0000

    Add venv setup script and gitignore

---
+++ env.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+virtualenv -p python3 ./id-generator-env
+source ./id-generator-env/bin/activate
+
+export CUDA=cu101
+
+pip install trimesh pyrender tqdm matplotlib rtree
+
+pip install torch==1.7.1+${CUDA} torchvision==0.8.2+${CUDA} -f https://download.pytorch.org/whl/torch_stable.html
+
+# install pytorch geometric
+export TORCH=1.7.0  # if torch 1.7.0 or 1.7.1
+pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
+pip install torch-sparse -f https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
+pip install torch-cluster -f https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
+pip install torch-spline-conv -f https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
+pip install torch-geometric
+
