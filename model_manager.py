ed9e16e671e22c467d46bfae7c952f08ed01e4fa
Simone <simofoti@gmail.com>
2021-04-01 17:20:10 +0100

    Store and load precomputed spirals and sampling transformations
    a

--- model_manager.py
+++ model_manager.py
@@ -1,39 +1,79 @@
+import os
+import pickle
 import torch.nn
 
 import utils
 from mesh_simplification import MeshSimplifier
+from compute_spirals import preprocess_spiral
 from model import AE
 
 
 class ModelManager(torch.nn.Module):
-    def __init__(self, configurations, device):
+    def __init__(self, configurations, device,
+                 precomputed_storage_path='precomputed'):
         super(ModelManager, self).__init__()
         self._model_params = configurations['model']
+        self._precomputed_storage_path = precomputed_storage_path
         self.device = device
-        self.template = utils.load_template(configurations['template_path'])
-        down_transforms, up_transforms = self._precompute_transformations()
-        spirals_indices = self._precompute_spirals()
+        self.template = utils.load_template(
+            configurations['data']['template_path'])
+        low_res_templates, down_transforms, up_transforms = \
+            self._precompute_transformations()
+        meshes_all_resolutions = [self.template] + low_res_templates
+        spirals_indices = self._precompute_spirals(meshes_all_resolutions)
         self.net = AE(in_channels=3, out_channels=3, latent_channels=None,
                       spiral_indices=spirals_indices,
                       down_transform=down_transforms,
                       up_transform=up_transforms)
 
     def _precompute_transformations(self):
-        sampling_params = self._model_params['sampling']
-        m = self.template
+        storage_path = os.path.join(self._precomputed_storage_path,
+                                    'transforms.pkl')
+        try:
+            with open(storage_path, 'rb') as file:
+                low_res_templates, down_transforms, up_transforms = \
+                    pickle.load(file)
+        except FileNotFoundError:
+            print("Computing Down- and Up- sampling transformations ")
+            if not os.path.isdir(self._precomputed_storage_path):
+                os.mkdir(self._precomputed_storage_path)
 
-        r_weighted = False if sampling_params['type'] == 'basic' else True
+            sampling_params = self._model_params['sampling']
+            m = self.template
 
-        low_res_templates = []
-        down_transforms = []
-        up_transforms = []
-        for sampling_factor in sampling_params['global_factors']:
-            simplifier = MeshSimplifier(in_mesh=m, debug=False)
-            m, down, up = simplifier(sampling_factor, r_weighted)
-            low_res_templates.append(m)
-            down_transforms.append(down)
-            up_transforms.append(up)
-        return down_transforms, up_transforms
+            r_weighted = False if sampling_params['type'] == 'basic' else True
 
-    def _precompute_spirals(self):
-        pass
+            low_res_templates = []
+            down_transforms = []
+            up_transforms = []
+            for sampling_factor in sampling_params['sampling_factors']:
+                simplifier = MeshSimplifier(in_mesh=m, debug=False)
+                m, down, up = simplifier(sampling_factor, r_weighted)
+                low_res_templates.append(m)
+                down_transforms.append(down)
+                up_transforms.append(up)
+
+            with open(storage_path, 'wb') as file:
+                pickle.dump(
+                    [low_res_templates, down_transforms, up_transforms], file)
+        return low_res_templates, down_transforms, up_transforms
+
+    def _precompute_spirals(self, templates):
+        storage_path = os.path.join(self._precomputed_storage_path,
+                                    'spirals.pkl')
+        try:
+            with open(storage_path, 'rb') as file:
+                spiral_indices_list = pickle.load(file)
+        except FileNotFoundError:
+            print("Computing Spirals")
+            spirals_params = self._model_params['spirals']
+            spiral_indices_list = []
+            for i in range(len(templates) - 1):
+                spiral_indices_list.append(
+                    preprocess_spiral(templates[i].face.t().cpu().numpy(),
+                                      spirals_params['length'][i],
+                                      templates[i].pos.cpu().numpy(),
+                                      spirals_params['dilation'][i]))
+            with open(storage_path, 'wb') as file:
+                pickle.dump(spiral_indices_list, file)
+        return spiral_indices_list
