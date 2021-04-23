100040ee0de936d2eb3df2fb4bcf1425b1b803fc
Simone <simofoti@gmail.com>
2021-04-23 13:54:24 +0100

    Create proper testing class

---
+++ test.py
@@ -0,0 +1,166 @@
+import os
+import pickle
+import tqdm
+import torch.nn
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from torchvision.io import write_video
+from torchvision.utils import make_grid, save_image
+
+
+class Tester:
+    def __init__(self, model_manager, norm_dict, data_loader, out_dir, config):
+        self._manager = model_manager
+        self._device = model_manager.device
+        self._norm_dict = norm_dict
+        self._out_dir = out_dir
+        self._config = config
+        self.latent_stats = self.compute_latent_stats(data_loader)
+
+    def set_renderings_size(self, size):
+        self._manager.renderer.rasterizer.raster_settings.image_size = size
+
+    def compute_latent_stats(self, data_loader):
+        storage_path = os.path.join(self._out_dir, 'z_stats.pkl')
+        try:
+            with open(storage_path, 'rb') as file:
+                z_stats = pickle.load(file)
+        except FileNotFoundError:
+            latents_list = []
+            for data in tqdm.tqdm(data_loader):
+                latents_list.append(self._manager.encode(
+                    data.x.to(self._device)).detach().cpu())
+            latents = torch.cat(latents_list, dim=0)
+            z_means = torch.mean(latents, dim=0)
+            z_stds = torch.std(latents, dim=0)
+            z_mins, _ = torch.min(latents, dim=0)
+            z_maxs, _ = torch.max(latents, dim=0)
+            z_stats = {'means': z_means, 'stds': z_stds,
+                       'mins': z_mins, 'maxs': z_maxs}
+
+            with open(storage_path, 'wb') as file:
+                pickle.dump(z_stats, file)
+        return z_stats
+
+    @staticmethod
+    def string_to_color(rgba_string, swap_bw=True):
+        rgba_string = rgba_string[1:-1]  # remove [ and ]
+        rgb_values = rgba_string.split()[:-1]
+        colors = [int(c) / 255 for c in rgb_values]
+        if colors == [1., 1., 1.] and swap_bw:
+            colors = [0., 0., 0.]
+        return tuple(colors)
+
+    def per_variable_range_experiments(self, z_range_multiplier=1):
+        z_means = self.latent_stats['means']
+        z_mins = self.latent_stats['mins'] * z_range_multiplier
+        z_maxs = self.latent_stats['maxs'] * z_range_multiplier
+
+        # Create video perturbing each latent variable from min to max.
+        # Show generated mesh and error map next to each other
+        # Frames are all concatenated along the same direction. A black frame is
+        # added before start perturbing the next latent variable
+        n_steps = 10
+        all_frames, all_rendered_differences, max_distances = [], [], []
+        for i in tqdm.tqdm(range(z_means.shape[0])):
+            z = z_means.repeat(n_steps, 1)
+            z[:, i] = torch.linspace(
+                z_mins[i], z_maxs[i], n_steps).to(self._device)
+            gen_verts = self._manager.generate(z.to(self._device)) * \
+                self._norm_dict['std'].to(self._device) + \
+                self._norm_dict['mean'].to(self._device)
+
+            differences_from_first = self._manager.compute_vertex_errors(
+                gen_verts, gen_verts[0].expand(gen_verts.shape[0], -1, -1))
+            max_distances.append(differences_from_first[-1, ::])
+            renderings = self._manager.render(gen_verts).cpu()
+            differences_renderings = self._manager.render(
+                gen_verts, differences_from_first, error_max_scale=5).cpu()
+            all_rendered_differences.append(differences_renderings)
+            frames = torch.cat([renderings, differences_renderings], dim=-1)
+            all_frames.append(
+                torch.cat([frames, torch.zeros_like(frames)[:2, ::]]))
+
+        write_video(
+            os.path.join(self._out_dir, 'latent_exploration.mp4'),
+            torch.cat(all_frames, dim=0).permute(0, 2, 3, 1) * 255, fps=4)
+
+        # Same video as before, but effects of perturbing each latent variables
+        # are shown in the same frame. Only error maps are shown.
+        grid_frames = []
+        grid_nrows = 8
+        if self._config['data']['swap_features']:
+            z_size = self._config['model']['latent_size']
+            grid_nrows = z_size // len(self._manager.latent_regions)
+
+        stacked_frames = torch.stack(all_rendered_differences)
+        for i in range(stacked_frames.shape[1]):
+            grid_frames.append(
+                make_grid(stacked_frames[:, i, ::], padding=10,
+                          pad_value=1, nrow=grid_nrows))
+        save_image(grid_frames[-1],
+                   os.path.join(self._out_dir, 'latent_exploration_tiled.png'))
+        write_video(
+            os.path.join(self._out_dir, 'latent_exploration_tiled.mp4'),
+            torch.stack(grid_frames, dim=0).permute(0, 2, 3, 1) * 255, fps=1)
+
+        # Create a plot showing the effects of perturbing latent variables in
+        # each region of the face
+        df = pd.DataFrame(columns=['mean_dist', 'z_var', 'region'])
+        df_row = 0
+        for zi, vert_distances in enumerate(max_distances):
+            for region, indices in self._manager.template.feat_and_cont.items():
+                regional_distances = vert_distances[indices['feature']]
+                mean_regional_distance = torch.mean(regional_distances)
+                df.loc[df_row] = [mean_regional_distance.item(), zi, region]
+                df_row += 1
+
+        sns.set_theme(style="ticks")
+        palette = {k: self.string_to_color(k) for k in
+                   self._manager.template.feat_and_cont.keys()}
+        grid = sns.FacetGrid(df, col="region", hue="region", palette=palette,
+                             col_wrap=4, height=3)
+
+        grid.map(plt.plot, "z_var", "mean_dist", marker="o")
+        plt.savefig(os.path.join(self._out_dir, 'latent_exploration.png'))
+
+
+if __name__ == '__main__':
+    import argparse
+    import utils
+    from data_generation_and_loading import get_data_loaders
+    from model_manager import ModelManager
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str,
+                        default='configurations/default.yaml',
+                        help="Path to the configuration file.")
+    parser.add_argument('--id', type=str, default='none',
+                        help="ID of experiment")
+    parser.add_argument('--output_path', type=str, default='.',
+                        help="outputs path")
+    opts = parser.parse_args()
+    configurations = utils.get_config(opts.config)
+    model_name = opts.id
+
+    output_directory = os.path.join(opts.output_path + "/outputs", model_name)
+    checkpoint_dir = os.path.join(output_directory, 'checkpoints')
+
+    if not torch.cuda.is_available():
+        device = torch.device('cpu')
+        print("GPU not available, running on CPU")
+    else:
+        device = torch.device('cuda')
+
+    manager = ModelManager(configurations=configurations, device=device)
+    manager.resume(checkpoint_dir)
+
+    train_loader, _, test_loader, normalization_dict = \
+        get_data_loaders(configurations, manager.template)
+
+    tester = Tester(manager, normalization_dict, train_loader,
+                    output_directory, configurations)
+    tester.set_renderings_size(512)
+    tester.per_variable_range_experiments()
